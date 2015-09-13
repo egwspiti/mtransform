ff4f3660b2e4a2ba345a4a56ade3669491ff685b
egwspiti <egwspiti@users.noreply.github.com>
2015-09-13 21:51:31 +0300

    as_is and rename dsl commands.

--- lib/mtransform/mtransform_dsl.rb
+++ lib/mtransform/mtransform_dsl.rb
@@ -1,12 +1,54 @@
 module Mtransform
   class MtransformDSL
+    attr_reader :input, :output, :cmds, :order
 
     def initialize(hash, &b)
+      @input  = hash
+      @output = {}
+      @cmds   = { as_is: [], rename: [] }
+      @order = []
+      instance_eval(&b)
+    end
 
+    def as_is(*keys)
+      raise ArgumentError unless keys.all? { |key| key.is_a? Symbol }
+      order << :as_is
+      cmds[:as_is] << keys
+    end
+
+    def rename(hash)
+      raise ArgumentError unless hash.respond_to?(:keys) && hash.respond_to?(:values) && hash.respond_to?(:all?)
+      raise ArgumentError unless (hash.keys + hash.values).all? { |key| key.is_a? Symbol }
+      order << :rename
+      cmds[:rename] << hash
     end
 
     def transform
+      order.each do |cmd|
+        case cmd
+        when :as_is
+          keys = cmds[:as_is].shift
+          run_as_is(keys)
+        when :rename
+          hash = cmds[:rename].shift
+          run_rename(hash)
+        end
+      end
+      output
+    end
 
+    private
+
+    def run_as_is(*keys)
+      keys.flatten.each do |key|
+        output[key] = input[key]
+      end
+    end
+
+    def run_rename(hash)
+      hash.each do |from, to|
+        output[to] = input[from]
+      end
     end
   end
 end
---
+++ spec/mtransform/mtransform_dsl_spec.rb
@@ -0,0 +1,104 @@
+require 'spec_helper'
+require 'mtransform/mtransform_dsl'
+
+module Mtransform
+  describe MtransformDSL do
+    let(:b) do
+      proc do
+        as_is :b, :c
+        rename :a => :x, :d => :y
+      end
+    end
+    let(:input) { { a: 1, b: 5, c: 'xxx' } }
+    subject { MtransformDSL.new(input, &b) }
+
+    context '.new' do
+      it 'takes a hash and a block' do
+        o = MtransformDSL.new(input, &b)
+        expect(o.input).to eq input
+      end
+
+      it 'reads as_is commands from the block' do
+        expect(subject.cmds[:as_is]).to eq [[:b, :c]]
+      end
+
+      it 'reads rename commands from the block' do
+        expect(subject.cmds[:rename]).to eq [{a: :x, d: :y}]
+      end
+    end
+
+    context '#transform' do
+      it 'transforms the input hash by running commands read from the block' do
+        expect(subject.transform).to eq ({ b: input[:b], c: input[:c], x: input[:a], y: input[:d] })
+      end
+
+      it 'commands are executed in the order they appear in the block' do
+        o = MtransformDSL.new(input) do
+          rename :a => :c
+          as_is :a, :c
+        end
+
+        f = MtransformDSL.new(input) do
+          as_is :a, :c
+          rename :a => :c
+        end
+
+        expect(o.transform).to eq ({a: input[:a], c: input[:c]})
+        expect(f.transform).to eq ({a: input[:a], c: input[:a]})
+      end
+    end
+
+    context 'DSL block' do
+      let(:b) do
+        proc { }
+      end
+      let(:input) { { a: 1, b: 5, c: 7, d: -1 } }
+
+      context '#as_is' do
+
+        it 'raises on non symbol args' do
+          expect { subject.as_is([:a, :b]) }.to raise_error(ArgumentError)
+        end
+
+        it 'output values at specified keys will be the values of input at these keys' do
+          subject.as_is(:a, :b)
+          expect(subject.transform).to eq ({a: input[:a], b: input[:b]})
+        end
+      end
+
+      context '#rename' do
+        let(:rename_hash) { {a: :e, b: :g } }
+
+        it 'raises on args that don\'t implement #keys' do
+          h = rename_hash.dup
+          h.instance_eval { undef :keys }
+
+          expect { subject.rename(h) }.to raise_error(ArgumentError)
+        end
+
+        it 'raises on args that don\'t implement #values' do
+          h = rename_hash.dup
+          h.instance_eval { undef :values }
+
+          expect { subject.rename(h) }.to raise_error(ArgumentError)
+        end
+
+        it 'raises on args that don\'t implement #all?' do
+          h = rename_hash.dup
+          h.instance_eval { undef :all? }
+
+          expect { subject.rename(h) }.to raise_error(ArgumentError)
+        end
+
+        it 'raises on args that don\'t have all #keys and #values to be Symbol' do
+          expect { subject.rename(a: String) }.to raise_error(ArgumentError)
+        end
+
+        it 'output value at arg\'s value will be the value of input at arg\'s key' do
+          subject.rename(rename_hash)
+          expect(subject.transform).to eq ({e: input[:a], g: input[:b]})
+        end
+      end
+    end
+  end
+end
